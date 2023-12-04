e02a9c6e33fbf7833659f1381380f41bc7d3b424
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-12-04 02:14:38 -0300

    0.85

--- app/src/main/java/com/Vitor5bonelli/orgs/base/model/Produto.kt
+++ app/src/main/java/com/Vitor5bonelli/orgs/base/model/Produto.kt
@@ -2,7 +2,7 @@ package com.Vitor5bonelli.orgs.base.model
 
 import java.math.BigDecimal
 
-class Produto (
+data class Produto (
     val nome: String,
     val descricao: String,
     val preco: BigDecimal
--- app/src/main/java/com/Vitor5bonelli/orgs/base/view/FormProductActivity.kt
+++ app/src/main/java/com/Vitor5bonelli/orgs/base/view/FormProductActivity.kt
@@ -2,7 +2,42 @@ package com.Vitor5bonelli.orgs.base.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.Vitor5bonelli.orgs.R
+import com.Vitor5bonelli.orgs.base.model.Produto
+import java.math.BigDecimal
 
-class FormProductActivity : AppCompatActivity(R.layout.activity_form_product) {
+class FormProductActivity : AppCompatActivity(R.layout.activity_form_product){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val saveButton = findViewById<Button>(R.id.saveBTN)
+        saveButton.setOnClickListener{
+            val nameField = findViewById<EditText>(R.id.nameET)
+            val descField = findViewById<EditText>(R.id.descET)
+            val priceField = findViewById<EditText>(R.id.priceEt)
+
+            val name = nameField.text.toString()
+            val desc = descField.text.toString()
+            val priceText = priceField.text.toString()
+
+            val price = if (priceText.isBlank()){
+                BigDecimal.ZERO
+            } else{
+                BigDecimal(priceText)
+            }
+
+            val novoProduto = Produto(
+                nome = name,
+                descricao = desc,
+                preco = price
+            )
+
+            Toast.makeText(this, "$novoProduto", Toast.LENGTH_LONG).show()
+        }
+    }
 }
